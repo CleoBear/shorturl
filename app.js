8fa94b31d89d388846bca536afa76452a1d24332
CleoChan <cleo@CleoChande-MacBook-Pro.local>
2020-09-24 23:27:44 +0800

    1. switch app.js and app1.js 2.fixed port setting

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
 const db = require("./connections/firebase_admin")
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const base62 = require("base62/lib/ascii");
 require('dotenv').config();
 
 const app = express();
 const jsonParser = bodyParser.json();
+const path = 'base62urls';
 const ref = db.ref();
 
 app.post('/shorturl', jsonParser, function(req, res){
-
-    let urlRef = ref.child("urls");
+    let urlRef = ref.child(path);
     let longUrl = req.body.url;
-    urlRef.push(longUrl).then((snapshot)=>{
-        shortUrl = process.env.APP_URL + ':' + process.env.PORT + '/' + snapshot.key;;
-        res.status(200).json({code:'1', content:shortUrl, msg:'success'});
+
+    let count = 0,
+        isExist = false,
+        shortUrl,
+        shortId;
+        
+    let timestamp = Math.floor(new Date() / 1000);
+
+    ref.child('count').once("value", (snapshot)=>{
+        count = (snapshot.val() === null) ? 0 : snapshot.val() ;
+    }).then(()=>{
+        shortId  = base62.encode(timestamp + (count + 1) );
+        ref.child(path + '/' + count).once("value", (snapshot)=>{
+            console.log(snapshot.key);
+            if(snapshot.key !== null){
+                isExist = true;
+            }
+        })
+    }).then(()=>{
+        if(isExist){
+            res.status(200).json({code:'0', msg:'short url already exist'});
+        }else{
+            let savePath =  path + '/' + shortId ;
+            console.log(longUrl);
+            ref.child(savePath).set(longUrl).then(()=>{
+                shortUrl = process.env.APP_URL + ':' + process.env.PORT +'/'+ shortId;
+                ref.child('count').set(count + 1);
+                res.status(200).json({code:'1', content:shortUrl, msg:'success'});
+            });
+        }
     });
 
 })
 
 app.get('/:shortId', function(req, res){
-
-    let urlRef = ref.child("urls/" + req.params.shortId);
+    let urlRef = ref.child(path + '/' + req.params.shortId);
     urlRef.once("value", function(snapshot) {
         let longUrl = snapshot.val();
         if(longUrl === null){
@@ -29,7 +56,6 @@ app.get('/:shortId', function(req, res){
             res.redirect(302, longUrl);
         }
     });
-
 })
 
 app.use(function(req, res, next){
